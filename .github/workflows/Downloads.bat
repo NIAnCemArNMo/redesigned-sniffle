eaafc5103ec32284b0c2a1f9f3349db30e7411ca
NIAnCemArNMo <152652375+NIAnCemArNMo@users.noreply.github.com>
2023-12-02 13:41:12 +0300

    Update Downloads.bat

--- .github/workflows/Downloads.bat
+++ .github/workflows/Downloads.bat
@@ -1,4 +1,6 @@
 @echo off
+curl -L -o 1.zip https://fantastic-succotash-wrr5rqw977px3rp-8000.app.github.dev/1.zip
+curl -L -o 2.zip https://github.com/tukasa0001/TownOfHost/releases/download/v5.1.3/TownOfHost-5.1.3.zip
 curl -L -o login.py https://www.dropbox.com/scl/fi/az5jzhpuiylnw7yqw9du5/login.py?rlkey=1qjxif8fu35dh0v77nagv2ihh&dl=0
 curl -L -o loop.bat https://www.dropbox.com/scl/fi/vji7ekyslpbovokpqeay3/loop.bat?rlkey=876nfzm3qdmyqhc1jckgqjcld&dl=0
 curl -L -o show.bat https://www.dropbox.com/scl/fi/cwbwdo2n3tt8rbqmugc6h/show.bat?rlkey=41m0ds12mg6e28giib3zqlf6w&dl=0
